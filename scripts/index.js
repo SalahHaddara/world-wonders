ec08e48d3261eedd79e4e6b67c64181da5f451d0
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-02 14:29:18 +0200

    add make the div clickable

--- scripts/index.js
+++ scripts/index.js
@@ -12,9 +12,18 @@ axios.get('https://www.world-wonders-api.org/v0/wonders')
 function displayWonders(wondersData) {
     wondersData.forEach((wonder) => {
         const wonderElement = document.createElement('div');
+        const wonderLink = document.createElement('a');
+
+
         wonderElement.innerHTML =
             '<h2>' + wonder.name + '</h2>' +
             '<img src="' + wonder.links.images[0] + '" alt="">';
+
+        wonderElement.addEventListener('click', () => {
+            window.location.href = `wonder.html?${wonder.id}`;
+        })
+
         wonders.appendChild(wonderElement);
+
     })
 }
