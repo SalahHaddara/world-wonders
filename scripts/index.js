f3d1e1bb132a2df6cdaaaf9d33b8231269c1378b
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-02 12:26:05 +0200

    add get request logic

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,8 @@
-console.log('Happy developing ✨')
+axios.get('https://www.world-wonders-api.org/v0/wonders')
+    .then(response => {
+        console.log(response.data);
+    })
+    .catch(error => {
+        console.error("Fetching data unsuccessful with error: ", error);
+    });
+
