95b2758a69be95113b3650af0ae3d3fefd9c5849
Salah Haddara <Salahhaddara02@gmail.com>
2024-11-02 13:42:22 +0200

    add display the fetched data

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,20 @@
+const wonders = document.getElementById('main');
+
 axios.get('https://www.world-wonders-api.org/v0/wonders')
     .then(response => {
         console.log(response.data);
+        displayWonders(response.data);
     })
     .catch(error => {
         console.error("Fetching data unsuccessful with error: ", error);
     });
 
+function displayWonders(wondersData) {
+    wondersData.forEach((wonder) => {
+        const wonderElement = document.createElement('div');
+        wonderElement.innerHTML =
+            '<h2>' + wonder.name + '</h2>' +
+            '<img src="' + wonder.links.images[0] + '" alt="">';
+        wonders.appendChild(wonderElement);
+    })
+}
